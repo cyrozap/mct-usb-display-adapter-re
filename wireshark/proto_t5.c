8a178f929c7bde631a5eec48a89022fd35fab259
cyrozap <cyrozap@gmail.com>
2026-09-12 00:38:20 -0500

    Dissect Trigger 5 bulk cursor flags byte

--- wireshark/proto_t5.c
+++ wireshark/proto_t5.c
@@ -112,6 +112,7 @@ static const value_string PIXEL_FMTS[] = {
 };
 
 static const true_false_string tfs_sync_polarity = { "Negative", "Positive" };
+static const true_false_string tfs_cursor_alpha_format = { "Special alpha values", "Normal alpha values" };
 
 static dissector_handle_t T5_HANDLE = NULL;
 
@@ -448,6 +449,10 @@ static int HF_T5_BULK_PAYLOAD_INFO = -1;
 static int HF_T5_BULK_PAYLOAD_FLAGS = -1;
 static int HF_T5_BULK_PAYLOAD_LEN = -1;
 static int HF_T5_BULK_OTHER_FLAGS = -1;
+static int HF_T5_BULK_CURSOR_FLAGS = -1;
+static int HF_T5_BULK_CURSOR_FLAGS_INDEX = -1;
+static int HF_T5_BULK_CURSOR_FLAGS_IMAGE = -1;
+static int HF_T5_BULK_CURSOR_FLAGS_ALPHA_FORMAT = -1;
 static int HF_T5_BULK_HEADER_CHECKSUM = -1;
 static int HF_T5_BULK_PAYLOAD_FRAGMENT = -1;
 static int HF_T5_BULK_REASSEMBLED_PAYLOAD = -1;
@@ -512,6 +517,22 @@ static hf_register_info HF_T5_BULK[] = {
     { &HF_T5_BULK_OTHER_FLAGS,
         { "Other flags", "trigger5.bulk.other_flags",
         FT_UINT8, BASE_HEX, NULL, 0x0, NULL, HFILL }
+    },
+    { &HF_T5_BULK_CURSOR_FLAGS,
+        { "Cursor flags", "trigger5.bulk.cursor_flags",
+        FT_UINT8, BASE_HEX, NULL, 0x0, NULL, HFILL }
+    },
+    { &HF_T5_BULK_CURSOR_FLAGS_ALPHA_FORMAT,
+        { "Cursor alpha format", "trigger5.bulk.cursor_flags.alpha_format",
+        FT_BOOLEAN, 8, TFS(&tfs_cursor_alpha_format), 0x20, NULL, HFILL }
+    },
+    { &HF_T5_BULK_CURSOR_FLAGS_IMAGE,
+        { "Cursor image payload", "trigger5.bulk.cursor_flags.image",
+        FT_BOOLEAN, 8, NULL, 0x10, NULL, HFILL }
+    },
+    { &HF_T5_BULK_CURSOR_FLAGS_INDEX,
+        { "Cursor index", "trigger5.bulk.cursor_flags.index",
+        FT_UINT8, BASE_DEC, NULL, 0x0F, NULL, HFILL }
     },
     { &HF_T5_BULK_HEADER_CHECKSUM,
         { "Header checksum", "trigger5.bulk.header_checksum",
@@ -609,6 +630,7 @@ static int ETT_T5_VIDEO_MODE_PLL_CONFIG = -1;
 static int ETT_T5_VIDEO_MODES = -1;
 static int ETT_T5_VIDEO_MODE_INFO = -1;
 static int ETT_T5_BULK_FRAME_INFO = -1;
+static int ETT_T5_BULK_CURSOR_FLAGS = -1;
 static int * const ETT[] = {
     &ETT_T5,
     &ETT_T5_FIRMWARE_VERSION,
@@ -618,6 +640,7 @@ static int * const ETT[] = {
     &ETT_T5_VIDEO_MODES,
     &ETT_T5_VIDEO_MODE_INFO,
     &ETT_T5_BULK_FRAME_INFO,
+    &ETT_T5_BULK_CURSOR_FLAGS,
     &ETT_T5_BULK_FRAGMENT,
     &ETT_T5_BULK_FRAGMENTS,
 };
@@ -951,6 +974,13 @@ static int handle_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *ptree, urb
         proto_tree_add_item(tree, HF_T5_BULK_PAYLOAD_FLAGS, tvb, 12, 4, ENC_LITTLE_ENDIAN);
         proto_tree_add_item(tree, HF_T5_BULK_PAYLOAD_LEN, tvb, 12, 4, ENC_LITTLE_ENDIAN);
         proto_tree_add_item(tree, HF_T5_BULK_OTHER_FLAGS, tvb, 16, 1, ENC_LITTLE_ENDIAN);
+
+        proto_item * cursor_flags_item = proto_tree_add_item(tree, HF_T5_BULK_CURSOR_FLAGS, tvb, 17, 1, ENC_NA);
+        proto_tree * cursor_flags_tree = proto_item_add_subtree(cursor_flags_item, ETT_T5_BULK_CURSOR_FLAGS);
+        proto_tree_add_item(cursor_flags_tree, HF_T5_BULK_CURSOR_FLAGS_ALPHA_FORMAT, tvb, 17, 1, ENC_LITTLE_ENDIAN);
+        proto_tree_add_item(cursor_flags_tree, HF_T5_BULK_CURSOR_FLAGS_IMAGE, tvb, 17, 1, ENC_LITTLE_ENDIAN);
+        proto_tree_add_item(cursor_flags_tree, HF_T5_BULK_CURSOR_FLAGS_INDEX, tvb, 17, 1, ENC_LITTLE_ENDIAN);
+
         uint32_t header_checksum = 0;
         proto_item * checksum_item = proto_tree_add_item_ret_uint(tree, HF_T5_BULK_HEADER_CHECKSUM, tvb, 19, 1, ENC_LITTLE_ENDIAN, &header_checksum);
         if (bulk_header_checksum_tvb_offset(tvb, 0, 19) != header_checksum) {
